abf5a8eb344e6d45d5a6ac8948d0103e34ac075e
Bruno <bruno@syligo.com>
2016-01-23 18:47:02 -0200

    facebook tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,10 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var List = require('./components/List.jsx');
 
+// Facebook tutorial
+var Hello = require('./components/FB/Hello.jsx');
+var Timer = require('./components/FB/Timer.jsx');
+
 ReactDOM.render(<List />, document.getElementById('ingredients'));
+ReactDOM.render(<Hello name='Bruno'/>, document.getElementById('hello'));
+ReactDOM.render(<Timer />, document.getElementById('timer'));
