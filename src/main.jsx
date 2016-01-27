56a499109969441b6d90aaed85db586cbaf7ea97
BrunoAl <brunoalnascimento@gmail.com>
2016-01-26 23:31:15 -0200

    cleaning

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,4 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var List = require('./components/List.jsx');
 
-// Facebook tutorial
-var Hello = require('./components/FB/Hello.jsx');
-var Timer = require('./components/FB/Timer.jsx');
-
 ReactDOM.render(<List />, document.getElementById('ingredients'));
